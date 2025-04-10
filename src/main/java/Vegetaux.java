16cc42dec1a6fb09f5f4e3e20c5c8e68a171c552
Picouleau Louis <plouvierlouis@gmail.com>
2025-04-10 11:06:27 +0200

    création végétaux et suppression fichiers inutiles

---
+++ src/main/java/Vegetaux.java
@@ -0,0 +1,35 @@
+public class Vegetaux {
+    private TypeVegetaux type;
+    private int posX;
+    private int posY;
+
+    public Vegetaux(TypeVegetaux type, int posX, int posY) {
+        this.type = type;
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public TypeVegetaux getType() {
+        return type;
+    }
+
+    public void setType(TypeVegetaux type) {
+        this.type = type;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
+}
